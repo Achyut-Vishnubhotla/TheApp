308fec4b39dcd20bd49fd5ec4431a2d9e96df8db
Sri Achyut Vishnubhotla <112319695+Achyut-Vishnubhotla@users.noreply.github.com>
2025-10-01 22:57:17 +0530

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,21 +1,69 @@
 import Image from "next/image";
-import { BsTwitter } from "react-icons/bs";
+import React from "react";
+import { BiHomeCircle, BiHash, BiUser } from "react-icons/bi";
+import { BsTwitter, BsBell, BsEnvelope, BsBookmark } from "react-icons/bs";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 interface AppSidebarButton {
-  
+  title: string;
+  icon: React.ReactNode;
 }
+
+const sidebarMenuItems: AppSidebarButton[] = [
+  {
+    title: "Home",
+    icon: <BiHomeCircle />,
+  },
+  {
+    title: "Explore",
+    icon: <BiHash />,
+  },
+  {
+    title: "Notifications",
+    icon: <BsBell />,
+  },
+  {
+    title: "Messages",
+    icon: <BsEnvelope />,
+  },
+  {
+    title: "Bookmarks",
+    icon: <BsBookmark />,
+  },
+  {
+    title: "Profile",
+    icon: <BiUser />,
+  }
+]
 
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-12 h-screen w-screen px-56">
-      <div className="col-span-3 flex justify-start pt-8">
-        <div className="text-4xl h-fit hover:bg-gray-800 rounded-full p-2 cursor-pointer transiion-all">
-          <BsTwitter />
-        </div>
-      </div>
-      <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-500"></div>
+    <div className={inter.className}>
+      <div className="grid grid-cols-12 h-screen w-screen px-56">
+        <div className="col-span-3 pt-8">
+          <div className="text-4xl h-fit w-fit hover:bg-gray-800 rounded-full p-4 cursor-pointer transiion-all">
+            <BsTwitter />
+            </div>
+            <div className="mt-4 text-2xl">
+              <ul>
+                {sidebarMenuItems.map((item) => (
+                  <li className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-5 py-2 w-fit cursor-pointer mt-2" key={item.title}>
+                    <span>{item.icon}</span>
+                    <span>{item.title}</span>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-5 pr-10">
+                <button className="bg-[#1d9bf0] p-4 font-semibold text-lg rounded-full w-full ">Tweet</button>
+              </div>
+            </div>
+          </div>
+        <div className="col-span-6 border-r-[1px] border-l-[1px] border-gray-500"></div>
       <div className="col-span-3"></div>
     </div>
+  </div>
   );
 }
